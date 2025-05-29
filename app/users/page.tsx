a46277c74ac8ae9c36f0c65a8cf6bdba8aa997f8
sathihskuamr M <msksathishzz@gmail.com>
2025-05-30 01:08:00 +0530

    add localtimeSting to check static and dynamic rendring

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -7,11 +7,12 @@ interface User {
 }
 
 const UserPage = async () => {
-    const res = await fetch('https://jsonplaceholder.typicode.com/users', {next: {revalidate: 10}})
+    const res = await fetch('https://jsonplaceholder.typicode.com/users', {cache: 'no-store'})
     const Users: User[] = await res.json();
   return (
     <div>
       <h1>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
       <ul>
         {Users.map(usr => <li key={usr.id}>{usr.name}</li>)}
       </ul>
